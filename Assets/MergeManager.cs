b36c502cd41459ded4a849857c861388b34dd917
SoftRiseGames <98230446+SoftRiseGames@users.noreply.github.com>
2025-06-11 21:03:15 +0300

    last

--- Assets/MergeManager.cs
+++ Assets/MergeManager.cs
@@ -29,7 +29,7 @@ public class MergeManager : MonoBehaviour
         UpgradedMaterialAdd.Clear();
         HashSet<GameObject> alreadyProcessedObjects = new HashSet<GameObject>();
 
-        // 1. aşama: Önce merge'e uygun objeler
+        // 1. Aşama: Upgrade olabilecek objeler
         foreach (GameObject handleCard in handledCardManager.HandledObjects)
         {
             if (alreadyProcessedObjects.Contains(handleCard))
@@ -39,84 +39,88 @@ public class MergeManager : MonoBehaviour
 
             foreach (var kvp in _items)
             {
-                if (inventoryObj.BaseItemObj == kvp.Value.RootMergeItem)
-                {
-                    List<GameObject> collideObjects = inventoryObj.CollideList;
-                    List<BaseItem> mergedItems = kvp.Value.MergedItems;
-                    List<BaseItem> collideBaseItems = new List<BaseItem>();
-                    Dictionary<BaseItem, GameObject> baseItemToObject = new Dictionary<BaseItem, GameObject>();
+                if (inventoryObj.BaseItemObj != kvp.Value.RootMergeItem)
+                    continue;
 
+                List<GameObject> collideObjects = inventoryObj.CollideList;
+                List<BaseItem> mergedItems = kvp.Value.MergedItems;
+
+                // Tüm objeleri (ana obje + çarpışanlar) topla
+                List<BaseItem> availableItems = new List<BaseItem>
+            {
+                inventoryObj.BaseItemObj // ana obje
+            };
+                Dictionary<BaseItem, List<GameObject>> itemToGameObjects = new();
+
+                foreach (GameObject obj in collideObjects)
+                {
+                    var baseItem = obj.GetComponent<IInventoryObject>().BaseItemObj;
+                    availableItems.Add(baseItem);
+
+                    if (!itemToGameObjects.ContainsKey(baseItem))
+                        itemToGameObjects[baseItem] = new List<GameObject>();
+
+                    itemToGameObjects[baseItem].Add(obj);
+                }
+
+                // MergedItems’teki item’ların gereksinimini kontrol et
+                List<BaseItem> requiredItems = new List<BaseItem>(mergedItems);
+                List<GameObject> usedColliders = new List<GameObject>();
+                bool allMatch = true;
+
+                foreach (BaseItem req in requiredItems.ToList())
+                {
+                    if (availableItems.Contains(req))
+                    {
+                        availableItems.Remove(req);
+                        requiredItems.Remove(req);
+
+                        // CollideList’ten geldiyse objeyi yakala
+                        if (itemToGameObjects.ContainsKey(req) && itemToGameObjects[req].Count > 0)
+                        {
+                            GameObject go = itemToGameObjects[req][0];
+                            usedColliders.Add(go);
+                            itemToGameObjects[req].RemoveAt(0);
+                        }
+                    }
+                    else
+                    {
+                        allMatch = false;
+                        break;
+                    }
+                }
+
+                if (allMatch && requiredItems.Count == 0)
+                {
+                    // Upgrade yapılabilir
+                    UpgradedMaterialAdd.Add(kvp.Value);
+                    alreadyProcessedObjects.Add(handleCard);
+
+                    foreach (var go in usedColliders)
+                        alreadyProcessedObjects.Add(go);
+
+                    // Geriye kalan CollideList'tekileri ekle
                     foreach (GameObject obj in collideObjects)
                     {
-                        var baseItem = obj.GetComponent<IInventoryObject>().BaseItemObj;
-                        collideBaseItems.Add(baseItem);
-                        if (!baseItemToObject.ContainsKey(baseItem))
-                            baseItemToObject[baseItem] = obj;
-                    }
-
-                    bool allRequiredItemsFound = false;
-                    List<BaseItem> tempMerged = new List<BaseItem>(mergedItems);
-                    List<GameObject> matchedCollideObjects = new List<GameObject>();
-
-                    foreach (BaseItem item in tempMerged.ToList())
-                    {
-                        bool found = false;
-                        for (int i = 0; i < collideBaseItems.Count; i++)
+                        if (!usedColliders.Contains(obj) && !alreadyProcessedObjects.Contains(obj))
                         {
-                            if (collideBaseItems[i] == item)
-                            {
-                                GameObject matchedObj = baseItemToObject[item];
-                                matchedCollideObjects.Add(matchedObj);
-                                collideBaseItems.RemoveAt(i);
-                                found = true;
-                                break;
-                            }
-                        }
-
-                        if (!found)
-                        {
-                            allRequiredItemsFound = false;
-                            break;
-                        }
-                        else
-                        {
-                            allRequiredItemsFound = true;
+                            UpgradedMaterialAdd.Add(obj.GetComponent<IInventoryObject>().BaseItemObj);
+                            alreadyProcessedObjects.Add(obj);
                         }
                     }
 
-                    if (allRequiredItemsFound && matchedCollideObjects.Count == mergedItems.Count)
-                    {
-                        UpgradedMaterialAdd.Add(kvp.Value);
-                        alreadyProcessedObjects.Add(handleCard);
-
-                        foreach (var matchedObj in matchedCollideObjects)
-                        {
-                            alreadyProcessedObjects.Add(matchedObj);
-                        }
-
-                        foreach (GameObject obj in collideObjects)
-                        {
-                            if (!matchedCollideObjects.Contains(obj) && !alreadyProcessedObjects.Contains(obj))
-                            {
-                                UpgradedMaterialAdd.Add(obj.GetComponent<IInventoryObject>().BaseItemObj);
-                                alreadyProcessedObjects.Add(obj);
-                            }
-                        }
-
-                        break; // Başka kombinasyona bakmaya gerek yok
-                    }
+                    break; // Başka kombinasyona gerek yok
                 }
             }
         }
 
-        // 2. aşama: Upgrade'e katılamayanları sıradan şekilde işle
+        // 2. Aşama: Upgrade’e katılamayanlar
         foreach (GameObject handleCard in handledCardManager.HandledObjects)
         {
             if (alreadyProcessedObjects.Contains(handleCard))
                 continue;
 
-            var inventoryObj = handleCard.GetComponent<IInventoryObject>();
-            UpgradedMaterialAdd.Add(inventoryObj.BaseItemObj);
+            UpgradedMaterialAdd.Add(handleCard.GetComponent<IInventoryObject>().BaseItemObj);
             alreadyProcessedObjects.Add(handleCard);
         }
     }
